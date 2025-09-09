7b8cf4bac1781ac069f5d44eec585b6eab46f5ec
adithyan@12789 <adithiruthiparambil12@gmail.com>
2025-09-09 14:31:45 +0530

    feat: :tada: First commit

---
+++ src/pages/_document.js
@@ -0,0 +1,45 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+        <Script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
+          strategy="beforeInteractive"
+        />
+        <Script src="https://code.jquery.com/jquery-3.6.4.min.js" strategy="lazyOnload" />
+        <Script
+          src="https://kit.fontawesome.com/e9f488f24b.js"
+          crossOrigin="anonymous"
+          strategy="lazyOnload"
+        />
+      </body>
+    </Html>
+  );
+}
---
+++ src/util/config.js
@@ -0,0 +1,3 @@
+export const baseURL = "http://localhost:3000";
+export const secretKey = "storyBox@192167";
+export const projectName = "StoryBox";
