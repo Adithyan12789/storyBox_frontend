9edd66a052ee6903bcad50e070b89f5c6ddcbd40
adithyan@12789 <adithiruthiparambil12@gmail.com>
2025-09-20 17:00:29 +0530

    privacy policy test - 9

--- src/privacy_policy/index.js
+++ src/privacy_policy/index.js
@@ -1,5 +1,5 @@
 "use client";
-import PrivacyPolicy from "../../component/PrivacyPolicy";
+import PrivacyPolicy from "../component/PrivacyPolicy";
 
 export default function PrivacyPolicyPage() {
   return <PrivacyPolicy />;
